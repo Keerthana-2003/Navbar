a034628ecd8ccaed3cb59c1984bc357d814e6c4f
Keerthana-2003 <93040835+Keerthana-2003@users.noreply.github.com>
2022-09-27 09:24:12 +0530

    Add files via upload

---
+++ profile/profile.js
@@ -0,0 +1,8 @@
+$(function () {
+    "use strict";
+    $(".pCard_add").click(function () {
+      $(".pCard_card").toggleClass("pCard_on");
+      $(".pCard_add i").toggleClass("fa-minus");
+    });
+  });
+  
